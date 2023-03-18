0d02ba9b192f4c2db71a66cc37e9064e33b25f45
rkdwoals159 <rkdwoals159@gmail.com>
2023-03-18 21:57:59 +0900

    feat: clubtable 생성

--- ajouthon/src/pages/activity/activityClub/ActivityClub.tsx
+++ ajouthon/src/pages/activity/activityClub/ActivityClub.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import ClubTable from "../../../components/ClubTale";
 
 const ActivityClub = () => {
   return (
     <div>
       <h1>This is ActivityClub page !</h1>
+      <ClubTable></ClubTable>
       <Outlet></Outlet>
     </div>
   );
